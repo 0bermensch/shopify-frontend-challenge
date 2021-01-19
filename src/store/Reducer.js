ff5cd65576a7bf69def9c2e16b67bec88d2163f1
Jason <jason.lo.kc@gmail.com>
2021-01-18 18:00:31 -0800

    fixed small bug

--- src/store/Reducer.js
+++ src/store/Reducer.js
@@ -1,4 +1,25 @@
-export default (state, action) => {
+// export default (state, action) => {
+//   switch (action.type) {
+//     case "ADD_MOVIE_TO_NOMINATELIST":
+//       return {
+//         ...state,
+//         nominatelist: [action.payload, ...state.nominatelist],
+//       };
+//     case "REMOVE_MOVIE_FROM_NOMINATELIST":
+//       return {
+//         ...state,
+//         nominatelist: state.nominatelist.filter(
+//           (info) => info.imdbID !== action.payload
+//         ),
+//       };
+//     default:
+//       return state;
+//   }
+// };
+
+import React from "react";
+
+const Reducer = (state, action) => {
   switch (action.type) {
     case "ADD_MOVIE_TO_NOMINATELIST":
       return {
@@ -16,3 +37,5 @@ export default (state, action) => {
       return state;
   }
 };
+
+export default Reducer;
