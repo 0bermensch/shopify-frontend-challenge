fecc681d8e952f6452de64d4f1860e75872ba9e8
Jason <jason.lo.kc@gmail.com>
2021-01-18 18:05:28 -0800

    commit

--- src/store/Reducer.js
+++ src/store/Reducer.js
@@ -1,24 +1,3 @@
-// export default (state, action) => {
-//   switch (action.type) {
-//     case "ADD_MOVIE_TO_NOMINATELIST":
-//       return {
-//         ...state,
-//         nominatelist: [action.payload, ...state.nominatelist],
-//       };
-//     case "REMOVE_MOVIE_FROM_NOMINATELIST":
-//       return {
-//         ...state,
-//         nominatelist: state.nominatelist.filter(
-//           (info) => info.imdbID !== action.payload
-//         ),
-//       };
-//     default:
-//       return state;
-//   }
-// };
-
-import React from "react";
-
 const Reducer = (state, action) => {
   switch (action.type) {
     case "ADD_MOVIE_TO_NOMINATELIST":
